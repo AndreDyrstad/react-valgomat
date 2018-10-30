8efd914888210ed5224798e160306b699623d9e7
Andre Dyrstad <dyrstad.a@gmail.com>
2018-10-30 15:37:04 +0100

    Connecte frontend -> API -> db

--- src/components/Forms.js
+++ src/components/Forms.js
@@ -50,6 +50,7 @@ class Forms extends Component {
 
     onSubmit = async values => {
         axios.post('http://modelling.hvl.no:8020/train', values)
+        //axios.post('http://localhost:5000/train', values)
             .then(res => this.setState({submitted: true, error: false}))
             .catch(err => this.setState({submitted: true, error: true}))
 
