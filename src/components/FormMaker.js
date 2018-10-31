ebcea5be75b08841db4f323a182d2f56196205f2
Andre Dyrstad <dyrstad.a@gmail.com>
2018-10-31 15:54:43 +0100

    Added a lot of styling

---
+++ src/components/FormMaker.js
@@ -0,0 +1,19 @@
+import React, {Component} from 'react'
+import {Form, Field} from 'react-final-form'
+import axios from "axios/index";
+import '../css/Header.css'
+import '../css/New.css'
+import {Button, Popover, OverlayTrigger, Glyphicon, Alert} from 'react-bootstrap'
+import center from '../json/centers'
+
+class FormMaker extends Component{
+
+    constructor(props) {
+        super(props);
+    }
+
+
+
+
+}
+export default FormMaker
