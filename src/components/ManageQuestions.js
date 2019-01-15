47b40e6b7bbde6382763c662d4a74f8669b46dc4
Andre Dyrstad <dyrstad.a@gmail.com>
2019-01-15 16:05:01 +0100

    Added the possibility to edit displayed questions

--- src/components/ManageQuestions.js
+++ src/components/ManageQuestions.js
@@ -8,18 +8,29 @@ class ManageQuestions extends Component {
     constructor(props) {
         super(props);
 
-        axios.get('http://localhost:5000/allQuestions').then(res => this.setState({files: res.data, isLoading: false}));
+        axios.get('http://localhost:5000/allQuestions').then(res => this.setState({files: res.data, isLoading: false})).then(res => console.log(this.state.files));
 
         this.state = {
             isLoading: true,
-            selectedQuestions: []
+            selectedQuestions: {},
+            categories: []
         }
     }
 
     addToList = (obj) => {
         let newState = this.state.selectedQuestions;
-        for(let i in obj.questions) newState.push(obj.questions[i]);
+
+        let current = this.state.files.questions;
+
+
+        for(let i in current) {
+            for(let j in obj.questions)
+            if (current[i].label === obj.questions[j])
+                newState[this.selectedCategory.value].push(current[i])
+        }
+
         this.setState({selectedQuestions: newState});
+
         console.log(this.state.selectedQuestions)
     };
 
@@ -28,7 +39,6 @@ class ManageQuestions extends Component {
         let newState = this.state.selectedQuestions;
         for(let i in newState){
             for(let j in obj.questions) {
-                console.log(newState[i] + "  " + obj.questions[j]);
                 if (newState[i] === obj.questions[j]){
                     newState.splice(i,1)
                 }
@@ -36,33 +46,82 @@ class ManageQuestions extends Component {
             }
         }
 
-        console.log(newState);
         this.setState({selectedQuestions: newState})
 
+    };
+
+    addCategory = (obj) => {
+        let newCategory = this.state.categories;
+        let newSelectedQuestions = this.state.selectedQuestions;
+
+        newCategory.push(obj.category);
+
+        newSelectedQuestions[obj.category] = [];
+
+        this.setState({categories: newCategory, selectedQuestions: newSelectedQuestions});
+
+    };
+
+    submit = () => {
+
+        axios.post('http://localhost:5000/updateQuestions', this.state.selectedQuestions)
     };
 
     showAllQuestions = () => (
         this.state.files.questions).map((obj, index) => {
 
         return (
-            <option key={obj.label}>{obj.label}</option>
+            <option id={obj.id} key={obj.label}>{obj.label}</option>
         )
 
     });
 
     showSelectedQuestions = () => (
-        this.state.selectedQuestions).map((obj, index) => {
-
+        this.state.selectedQuestions[this.selectedCategory.value]).map((obj, index) => {
         return (
-            <option key={obj}>{obj}</option>
+            <option key={obj.id}>{obj.label}</option>
         )
 
     });
+
+    showCategories = () => (
+        this.state.categories).map((obj, index) => {
+
+            return (
+                <option key={obj}>{obj}</option>
+            )
+
+        }
+    );
 
 
     render() {
         return (
             <div>
+                <h1>Legg til kategori</h1>
+                <div>
+                    <Form
+                        onSubmit={this.addCategory}
+                        render={({handleSubmit, form, submitting, pristine, values}) => (
+                            <form onSubmit={handleSubmit}>
+                                        <label>
+                                            Legg til en ny kategori
+                                            <Field
+                                                name="category"
+                                                component="input"
+                                                type="text"
+                                                value="category"
+                                            />{' '}
+                                        </label>
+                                <div className="buttons">
+                                    <button type="submit" disabled={submitting || pristine}>
+                                        Submit
+                                    </button>
+                                </div>
+                            </form>
+                        )}
+                    />
+                </div>
                 <h1>Alle spørsmål</h1>
                 <div>
                 <Form
@@ -81,6 +140,11 @@ class ManageQuestions extends Component {
                     )}
                 />
                 </div>
+                <div>
+                    <select onChange={() => this.forceUpdate()} ref = {(input)=> this.selectedCategory = input}>
+                        {this.showCategories()}
+                    </select>
+                </div>
                 <h1>Valgte spørsmål</h1>
                 <div>
                     <Form
@@ -88,7 +152,7 @@ class ManageQuestions extends Component {
                         render={({handleSubmit, form, submitting, pristine, values}) => (
                             <form onSubmit={handleSubmit}>
                                 <Field name="questions" component="select" type="select" multiple>
-                                    {this.state.isLoading ? null : this.showSelectedQuestions()}
+                                    {this.state.isLoading || this.selectedCategory.value === "" ? null : this.showSelectedQuestions()}
                                 </Field>
                                 <div className="buttons">
                                     <button type="submit" disabled={submitting || pristine}>
@@ -101,7 +165,7 @@ class ManageQuestions extends Component {
                 </div>
 
 
-
+                <button onClick={this.submit}>Oppdater spørsmålslisten</button>
             </div>
 
         )
